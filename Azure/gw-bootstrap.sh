0202b03c821a06e04186919a5e4ed3f22b019e65
Michael Braun <michaelbraunbass@gmail.com>
2020-06-08 13:39:36 -0600

    Update gw-bootstrap.sh

--- Azure/gw-bootstrap.sh
+++ Azure/gw-bootstrap.sh
@@ -3,4 +3,4 @@ touch /home/admin/test.txt
 
 clish -c 'set user admin shell /bin/bash' -s
 
-config_system -s 'install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=true&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=R80dot40mgmt&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin'
+config_system -s 'install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin&ftw_sic_key=Vpn123vpn123'
