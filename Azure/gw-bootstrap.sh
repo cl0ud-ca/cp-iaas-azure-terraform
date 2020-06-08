0a02428170111a25b14ab173cff32fa97f4d8af1
Michael Braun <michaelbraunbass@gmail.com>
2020-06-08 13:37:00 -0600

    Test Update

--- Azure/gw-bootstrap.sh
+++ Azure/gw-bootstrap.sh
@@ -1,12 +1,6 @@
-#!/usr/bin/python3 /etc/cloud_config.py
+#!/bin/bash
+touch /home/admin/test.txt
 
+clish -c 'set user admin shell /bin/bash' -s
 
-installationType="gateway"
-bootstrapScript64=""
-templateName="gateway"
-templateVersion="20200508"
-isBlink="false"
-osVersion="R8040"
-sicKey="Vpn123vpn123"
-managementGUIClientNetwork="0.0.0.0/0"
-allowUploadDownload="True"
+config_system -s 'install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=true&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=R80dot40mgmt&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin'
