90baee5abd18a5888940cbad47906704d43e96ad
Michael Braun <michaelbraunbass@gmail.com>
2020-06-10 07:55:14 -0600

    Reorg

--- Azure/resource_groups.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "cp-mgmt-rg" {
-  name = "cp-mgmt-rg"
-  location = "West US 2"
-}
-
--- Check_Point/main-azure.tf
+++
@@ -1,5 +0,0 @@
-provider "azurerm" {
-    version = "=2.1.0"
-    features {}
-    }
-    
