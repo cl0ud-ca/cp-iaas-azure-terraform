6f4ae171e31b89ae519602c032656dc7f53c38a3
Michael Braun <michaelbraunbass@gmail.com>
2020-06-10 08:01:38 -0600

    Update random.tf

--- random.tf
+++ random.tf
@@ -2,7 +2,7 @@
 resource "random_id" "randomId2" {
     keepers = {
         # Generate a new ID only when a new resource group is defined
-        resource_group = "${azurerm_resource_group.cp-mgmt-rg.name}"
+        resource_group = "${azurerm_resource_group.cp-gw-rg.name}"
     }
 
     byte_length = 8
