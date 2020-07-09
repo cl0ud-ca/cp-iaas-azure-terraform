18a94acf903f3bd311ef901c7f24f359ba06cd0a
cl0udzilla <68074193+cl0udzilla@users.noreply.github.com>
2020-07-09 14:18:20 -0500

    New Update
    New Summary

--- gw-bootstrap.sh
+++ gw-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #This build the config string
-config_string='install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin&ftw_sic_key='"$sic_key"''
+config_string='install_security_gw=true&install_ppak=true&gateway_cluster_member=false&install_security_managment=false&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname='"$hostname"'-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin&ftw_sic_key='"$sic_key"''
 
 clish -c 'set user admin shell /bin/bash' -s
 
--- resource_groups.tf
+++ resource_groups.tf
@@ -1,5 +1,5 @@
 
 resource "azurerm_resource_group" "cp-gw-rg" {
-  name = "cp-gw-rg"
+  name = "${var.company}-rg"
   location = var.location
 }
